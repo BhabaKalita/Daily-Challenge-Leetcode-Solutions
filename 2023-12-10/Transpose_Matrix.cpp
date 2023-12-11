f13bc0344a33ca8e0a7f2c625b0c3d3af5f0e9d7
B K <8bhaba8@gmail.com>
2023-12-11 01:51:58 +0000

    10 Dec 2023 Daily Question

---
+++ 2023-12-10/Transpose_Matrix.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  // Function to calculate the transpose of a given matrix
+  vector<vector<int>> transpose(vector<vector<int>> &matrix) {
+    // Get the number of rows and columns in the original matrix
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    // Create a matrix to store the transpose with swapped rows and columns
+    vector<vector<int>> res(col, vector<int>(row, 0));
+
+    // Fill in the transpose matrix by swapping rows and columns
+    for (int i = 0; i < col; i++) {
+      for (int j = 0; j < row; j++) {
+        res[i][j] = matrix[j][i];
+      }
+    }
+    // Return the transposed matrix
+    return res;
+  }
+};

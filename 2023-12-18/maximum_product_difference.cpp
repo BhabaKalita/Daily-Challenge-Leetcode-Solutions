6dc78ad370fbdf59ff6b99599dd2f7cac9a3b6eb
B K <8bhaba8@gmail.com>
2023-12-18 03:26:26 +0000

    18 Dec 2023 Daily Question

---
+++ 2023-12-18/maximum_product_difference.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <limits.h> // For INT_MIN and INT_MAX
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxProductDifference(vector<int> &nums) {
+    // Initialize variables for first and second highest, and first and second
+    // lowest
+    int firstH = INT_MIN, secondH = INT_MIN, firstL = INT_MAX,
+        secondL = INT_MAX;
+    int n = nums.size(); // Get the size of the vector
+
+    // Find the first and second highest elements
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] > firstH) {
+        secondH = firstH;
+        firstH = nums[i];
+      } else if (nums[i] > secondH) {
+        secondH = nums[i];
+      }
+    }
+
+    // If all elements are the same, set secondH to firstH
+    if (secondH == INT_MIN)
+      secondH = firstH;
+
+    // Find the first and second lowest elements
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] < firstL) {
+        secondL = firstL;
+        firstL = nums[i];
+      } else if (nums[i] < secondL) {
+        secondL = nums[i];
+      }
+    }
+
+    // If all elements are the same, set secondL to firstL
+    if (secondL == INT_MAX)
+      secondL = firstL;
+
+    // Calculate and return the result
+    return (firstH * secondH) - (firstL * secondL);
+  }
+};

d8a770a85a68c3b10aac580460392f0310bfc82c
B K <8bhaba8@gmail.com>
2023-12-23 03:35:49 +0000

    23 Dec 2023 Daily Question.

--- 2023-12-23/path_crossing.cpp
+++ 2023-12-23/path_crossing.cpp
@@ -1,36 +1,45 @@
 #include <set>
-#include <utility>
 #include <string>
+#include <utility>
 
 class Solution {
 public:
-    /**
-     * Check if the given path crosses itself.
-     * @param path The input path as a string containing directions (N, S, E, W).
-     * @return True if the path crosses itself, false otherwise.
-     */
-    bool isPathCrossing(string path) {
-        // Set to store visited points
-        set<pair<int, int>> temp;
+  /**
+   * Check if the given path crosses itself.
+   * @param path The input path as a string containing directions (N, S, E, W).
+   * @return True if the path crosses itself, false otherwise.
+   */
+  bool isPathCrossing(string path) {
+    // Set to store visited points
+    set<pair<int, int>> temp;
 
-        // Initial position
-        int x = 0, y = 0;
-        temp.insert({0, 0});
+    // Initial position
+    int x = 0, y = 0;
+    temp.insert({0, 0});
 
-        // Traverse the path
-        for (char ch : path) {
-            // Update the position based on the current direction
-            if (ch == 'N') ++y;
-            else if (ch == 'S') --y;
-            else if (ch == 'E') ++x;
-            else --x;
+    // Traverse the path
+    for (char ch : path) {
+      // Update the position based on the current direction
+      if (ch == 'N')
+        ++y;
+      else if (ch == 'S')
+        --y;
+      else if (ch == 'E')
+        ++x;
+      else
+        --x;
 
-            // Check if the current position has been visited before
-            if (!temp.insert({x, y}).second)
-                return true;
-        }
-
-        // If no crossing is found
-        return false;
+      // Check if the current position has been visited before
+      if (!temp.insert({x, y}).second)
+        return true;
     }
+
+    // If no crossing is found
+    return false;
+  }
 };
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/

a9e8f7f7677235851fba6e08621ffb61a7c0241c
B K <8bhaba8@gmail.com>
2024-02-23 03:33:28 +0000

    23 Feb 2024 Daily Question.

--- 2024/February 2024/2024-02-23/cheapest_flights_within_k_stops.cpp
+++ 2024/February 2024/2024-02-23/cheapest_flights_within_k_stops.cpp
@@ -3,9 +3,9 @@
 
 class Solution {
 public:
-    int findCheapestPrice(int n, std::vector<std::vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         // Initialize a vector to store the minimum cost to reach each node.
-        std::vector<int> c(n, 1e8); // Initialize all costs to a large value (infinite).
+        vector<int> c(n, 1e8); // Initialize all costs to a large value (infinite).
 
         // The cost to reach the source node is 0.
         c[src] = 0;
@@ -13,12 +13,12 @@ public:
         // Iterate for k+1 times as we are allowed to make k stops.
         for(int i = 0; i <= k; i++) {
             // Create a temporary vector to store updated costs.
-            std::vector<int> temp(c);
+            vector<int> temp(c);
 
             // Iterate over each flight.
             for(auto x : flights) {
                 // Update the cost to reach the destination node of the flight if it reduces the cost.
-                temp[x[1]] = std::min(temp[x[1]], c[x[0]] + x[2]);
+                temp[x[1]] = min(temp[x[1]], c[x[0]] + x[2]);
             }
 
             // Update the main cost vector with the temporary one.

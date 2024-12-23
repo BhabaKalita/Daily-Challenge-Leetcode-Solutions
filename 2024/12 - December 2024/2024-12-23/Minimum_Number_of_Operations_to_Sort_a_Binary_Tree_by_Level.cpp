4b0bb8b89bfeed72ad52bdc1522cb7453e6801dc
Bhaba K <76607446+BhabaKalita@users.noreply.github.com>
2024-12-23 13:54:10 +0530

    23 December 2024 Daily Question

---
+++ 2024/12 - December 2024/2024-12-23/Minimum_Number_of_Operations_to_Sort_a_Binary_Tree_by_Level.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countSwap(vector<int>& t){
+        int count = 0;
+        map<int,int> mp;
+        vector<int> u;
+        for(int i = 0; i < t.size(); ++i){
+            mp[t[i]] = i;
+            u.push_back(t[i]);
+        }
+        sort(u.begin(), u.end());
+        for(int i = 0; i < t.size(); ++i){
+            if(t[i] != u[i] ){  
+                t[mp[u[i]]] = t[i];
+                mp[t[i]] = mp[u[i]];
+                mp[u[i]] = i;
+                t[i] = u[i];
+                count++;
+            }
+        }
+        return count;
+    }
+    int minimumOperations(TreeNode* root) {
+        int ans = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> t;
+            while(n--){
+                TreeNode* node = q.front();
+                t.push_back(node->val);
+                q.pop();
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+            }
+            for(auto c: t) cout<<c<<" "; cout<<endl;
+            ans += countSwap(t);
+        }
+        return ans;
+    }
+};
+// Time Complexity: O(Nlog⁡N)
+// Space Complexity: O(N)
